2b09254643985b6f8c6acd46d6d0cf314cd1bbb3
Danishwer _ <dannywits12321@gmail.com>
2024-09-04 19:41:15 +0530

    minor fixes

--- a1.java
+++ a1.java
@@ -4,10 +4,10 @@ public class a1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         final double PI = Math.PI;
-        System.out.print("Enter the radius of the circle : ");
+        System.out.print("Enter the radius : ");
         double radius = scanner.nextDouble();
         double area = radius * radius * PI;
-        System.out.printf("The area of the circle is : %.2f", area);
+        System.out.printf("The area is : %.2f", area);
         scanner.close();
     }
 }
